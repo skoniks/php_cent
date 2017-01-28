7f3fd71d09203a54fc7d559da9789b25edcbd16d
SKONIKS <skolya16@ya.ru>
2017-01-28 23:39:29 +0300

    Code improvements, support genToken

--- src/Clients/AbstractClient.php
+++ src/Clients/AbstractClient.php
@@ -6,12 +6,6 @@ use SKONIKS\Centrifuge\Contracts\Centrifuge;
 
 abstract class AbstractClient implements Centrifuge
 {
-    /**
-     * @param $channel
-     * @param $data
-     * @param null|string $client
-     * @return mixed
-     */
     public function publish($channel, $data, $client = null)
     {
         $params = [
@@ -21,12 +15,6 @@ abstract class AbstractClient implements Centrifuge
         $isClient = empty($client) ? [] : ['client' => $client];
         return $this->send('publish', array_merge($params, $isClient));
     }
-
-    /**
-     * @param string $channel
-     * @param string $user
-     * @return mixed
-     */
     public function unsubscribe($channel, $user)
     {
         return $this->send('unsubscribe', [
@@ -34,44 +22,31 @@ abstract class AbstractClient implements Centrifuge
             'user' => $user,
         ]);
     }
-
-    /**
-     * @param string $user
-     * @return mixed
-     */
     public function disconnect($user)
     {
         return $this->send('disconnect', [
             'user' => $user,
         ]);
     }
-
-    /**
-     * @param string $channel
-     * @return mixed
-     */
     public function presence($channel)
     {
         return $this->send('presence', [
             'channel' => $channel,
         ]);
     }
-
-    /**
-     * @param string $channel
-     * @return mixed
-     */
     public function history($channel)
     {
         return $this->send('history', [
             'channel' => $channel,
         ]);
     }
-
-    /**
-     * @param string $method
-     * @param array $params
-     * @return mixed
-     */
+    public function generateToken($userId, $timestamp, $info = '')
+    {
+        $ctx = hash_init('sha256', HASH_HMAC, config('centrifuge.secret'));
+        hash_update($ctx, $userId);
+        hash_update($ctx, $timestamp);
+        hash_update($ctx, $info);
+        return hash_final($ctx);
+    }
     abstract protected function send($method, $params);
 }
--- src/Contracts/Centrifuge.php
+++ src/Contracts/Centrifuge.php
@@ -4,48 +4,10 @@ namespace SKONIKS\Centrifuge\Contracts;
 
 interface Centrifuge
 {
-
-    /**
-     * @param $channel
-     * @param $data
-     * @param null|string $client
-     * @return mixed
-     */
     public function publish($channel, $data, $client = null);
-
-    /**
-     * @param string $channel
-     * @param string $user
-     * @return mixed
-     */
     public function unsubscribe($channel, $user);
-
-    /**
-     * @param string $user
-     * @return mixed
-     */
     public function disconnect($user);
-
-    /**
-     * @param string $channel
-     * @return mixed
-     */
     public function presence($channel);
-
-    /**
-     * @param string $channel
-     * @return mixed
-     */
     public function history($channel);
-    
-    public function generateToken($userId, $timestamp, $info = '')
-    {
-        $ctx = hash_init('sha256', HASH_HMAC, config('centrifuge.secret'));
-        hash_update($ctx, $userId);
-        hash_update($ctx, $timestamp);
-        hash_update($ctx, $info);
-
-        return hash_final($ctx);
-    }
-    
+    public function generateToken($userId, $timestamp, $info = '');
 }
